71d9709f61943e3e0b315e59b13c8a96b0cfa6d3
GerryCampa <56122434+GerryCampa@users.noreply.github.com>
2020-02-27 18:25:28 +0100

    java

---
+++ workspace/iniciales/src/primero/Holamundo.java
@@ -0,0 +1,10 @@
+package primero;
+
+public class Holamundo {
+
+	public static void main(String[] args) {
+		// my first program
+		System.out.println("Hola mundo");
+	}
+
+}
---
+++ workspace/iniciales/src/primero/Nota.java
@@ -0,0 +1,23 @@
+package primero;
+
+import java.util.Scanner;
+
+public class Nota {
+	
+	public static void main(String[] args){
+
+	
+	double nota;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Introduzca la nota del alumno");
+	nota=sc.nextDouble();
+	
+	if (nota < 5) {
+		  System.out.println("El alumno está suspenso, solo ha sacado "+nota);
+		} else if(nota>=5 && nota<=8.5){
+		  System.out.println("El alumno está aprobado, ha sacado un  "+nota);
+		}else{
+			System.out.println("El alumno tiene un sobresaliente, ha sacado un "+nota);
+		}
+}
+}
